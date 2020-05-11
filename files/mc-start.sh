1ccdfbdf28105a5459a6c397b7d882484503d934
Cristina Sirbu <cristina.sirbu@ness.com>
2020-05-11 06:55:57 +0300

    Review comments

--- files/mc-start.sh
+++ files/mc-start.sh
@@ -38,8 +38,12 @@ fi
 
 if [ -n "${MC_ADMIN_USER}" ] && [ -n "${MC_ADMIN_PASSWORD}" ]; then
   echo "Creating admin user"
-  source "mc-conf.sh user create -H=${MC_DATA} -n=${MC_ADMIN_USER} -p=${MC_ADMIN_PASSWORD} -r=admin"
-  exit $?
+  source ./mc-conf.sh user create -H=${MC_DATA} -n=${MC_ADMIN_USER} -p=${MC_ADMIN_PASSWORD} -r=admin
+  if [ $? -eq 0 ]; then
+    echo "User created successfully"
+  else
+    exit 1
+  fi
 fi
 
 echo "##################################################"
