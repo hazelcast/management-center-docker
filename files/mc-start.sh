f2e1fb3122599e170fced511cf86f20c9c41164f
emre <emre@hazelcast.com>
2020-10-15 10:27:37 +0300

    Address review comments - print diagnostic messages to stderr

--- files/mc-start.sh
+++ files/mc-start.sh
@@ -14,7 +14,7 @@ if [ -n "${LOGGING_LEVEL}" ]; then
 fi
 
 if [ "${CONTAINER_SUPPORT:-true}" = "false" ] ;then
-    echo "Container support disabled. Using manual heap sizing by specifying MIN_HEAP_SIZE, MAX_HEAP_SIZE or custom settings configured by JAVA_OPTS."
+    echo "Container support disabled. Using manual heap sizing by specifying MIN_HEAP_SIZE, MAX_HEAP_SIZE or custom settings configured by JAVA_OPTS." 1>&2
     if [ -n "${MIN_HEAP_SIZE}" ]; then
         export JAVA_OPTS="${JAVA_OPTS} -Xms${MIN_HEAP_SIZE}"
     fi
@@ -22,7 +22,7 @@ if [ "${CONTAINER_SUPPORT:-true}" = "false" ] ;then
         export JAVA_OPTS="${JAVA_OPTS} -Xmx${MAX_HEAP_SIZE}"
     fi
 else
-    echo "Container support enabled. Using automatic heap sizing. JVM will use up to 80% of the memory limit of the container."
+    echo "Container support enabled. Using automatic heap sizing. JVM will use up to 80% of the memory limit of the container." 1>&2
     export JAVA_OPTS="${JAVA_OPTS} -XX:+UseContainerSupport -XX:MaxRAMPercentage=80"
 fi
 
@@ -34,30 +34,30 @@ else
 fi
 
 if [ -n "${MC_INIT_CMD}" ]; then
-   echo "Executing command specified by MC_INIT_CMD for container initialization."
+   echo "Executing command specified by MC_INIT_CMD for container initialization." 1>&2
    eval "${MC_INIT_CMD}"
 fi
 
 if [ -n "${MC_INIT_SCRIPT}" ]; then
-    echo "Loading script $MC_INIT_SCRIPT specified by MC_INIT_SCRIPT for container initialization."
+    echo "Loading script $MC_INIT_SCRIPT specified by MC_INIT_SCRIPT for container initialization." 1>&2
     # shellcheck source=/dev/null
     source "${MC_INIT_SCRIPT}"
 fi
 
 if [ -n "${MC_ADMIN_USER}" ] && [ -n "${MC_ADMIN_PASSWORD}" ]; then
-  echo "Creating admin user."
+  echo "Creating admin user."  1>&2
   source ./mc-conf.sh user create --lenient=true -H="${MC_DATA}" -n="${MC_ADMIN_USER}" -p="${MC_ADMIN_PASSWORD}" -r=admin
   # shellcheck disable=SC2181
   if [ $? -eq 0 ]; then
-    echo "User created successfully."
+    echo "User created successfully." 1>&2
   else
     exit 1
   fi
 fi
 
-echo "##################################################"
-echo "# Initialisation complete, starting now....      #"
-echo "##################################################"
+echo "##################################################" 1>&2
+echo "# Initialisation complete, starting now....      #" 1>&2
+echo "##################################################" 1>&2
 
 # --add-opens flag is required to prevent this issue: https://jira.spring.io/browse/SPR-15859
 set -x
